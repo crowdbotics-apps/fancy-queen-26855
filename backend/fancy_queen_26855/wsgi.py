aa9b37f14637bc51c6cb0097a22e7492099b8151
Crowdbotics <team@crowdbotics.com>
2021-05-18 08:40:08 +0000

    Initial commit.

---
+++ backend/fancy_queen_26855/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fancy_queen_26855 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fancy_queen_26855.settings')
+
+application = get_wsgi_application()
